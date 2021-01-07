a07f60859f88629d13f6f922b3561b88490f376d
VincentVallerich <vallerichvincent@outlook.fr>
2021-01-07 15:46:28 +0100

    Update

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 int main(int argc, char const *argv[])
 {
     if (argc <= 2) printf("Usage : command argument(s)");
-    if (argv[0] == "cpstd") printf("salut");
-    if (argv[0] == "cpstd") cp_file(argc, argv[1], argv[2]);
-    if (argv[0] == "append") append_to_file(argc, argv[2]);
+    if (strcmp(argv[1], "cpstd") == 0) cp_file(argc, argv[2], argv[3]);
+    if (strcmp(argv[1], "append") == 0) append_to_file(argc, argv[2]);
     return 0;
 }
