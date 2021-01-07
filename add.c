1112a374acbc5ce08bafaa551c22448c3406e66c
VincentVallerich <vallerichvincent@outlook.fr>
2021-01-07 15:26:09 +0100

    Fix

--- add.c
+++ add.c
@@ -1,19 +1,17 @@
 #include "add.h"
 
-void append_to_file(int argc, char *argv[]) {
+void append_to_file(int argc, const char *src) {
 	FILE *s;
 	unsigned int c;
 	char msg[80];
  
-	if (argc != 3) usage(argc,argv);
 	printf("Ajout par fonctions de librairie standard\n");
-	if ((s=fopen(argv[1],"r"))==NULL) {
-		sprintf(msg,"Ouverture du fichier « %s »",argv[1]);
+	if ((s=fopen(src,"a"))==NULL) {
+		sprintf(msg,"Ouverture du fichier « %s »",src);
   		exit_error(msg);
 	}
 	while ((c=getc(s)) !=EOF) {
 		if (putc(c,s) != c) exit_error("Erreur sur putc");
 	}
-	if (ferror(sf)) exit_error("Erreur lors de la copie");
 	fclose(s);
 }
--- add.h
+++ add.h
@@ -1,6 +1,12 @@
 #ifndef ADD_H
 #define ADD_H
 
-void append_to_file(int argc, char *argv[]);
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "debug.h"
+
+void append_to_file(int argc, const char *src);
 
 #endif
