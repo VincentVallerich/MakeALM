2ed6450e6a56aa5eca5f13382293c77ba119cad9
Simon Moreau <simon.moreau@uha.fr>
2021-01-07 18:07:14 +0100

    q43

---
+++ phase2/app/build/CMakeFiles/traitement.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/src/add.c" "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/build/CMakeFiles/traitement.dir/src/add.c.o"
+  "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/src/cpstd.c" "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/build/CMakeFiles/traitement.dir/src/cpstd.c.o"
+  "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/src/debug.c" "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/build/CMakeFiles/traitement.dir/src/debug.c.o"
+  "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/src/main.c" "/home/simon/Windows/Users/lesis/Documents/ENSISA/2A/Semestre 1/Adéquation Logiciel Matériel/TP/tp1/phase2/app/build/CMakeFiles/traitement.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "~app/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ phase2/app/build/CMakeFiles/traitement.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traitement.dir/src/add.c.o"
+  "CMakeFiles/traitement.dir/src/cpstd.c.o"
+  "CMakeFiles/traitement.dir/src/debug.c.o"
+  "CMakeFiles/traitement.dir/src/main.c.o"
+  "traitement.pdb"
+  "traitement"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/traitement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
