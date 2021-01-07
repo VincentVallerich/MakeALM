e74c388d80820e3f1ed16f1d5c343be8f91c0aea
Simon Moreau <simon.moreau@uha.fr>
2021-01-07 14:59:45 +0100

    fonction add

---
+++ add.h
@@ -0,0 +1,6 @@
+#ifndef ADD_H
+#define ADD_H
+
+void append_to_file(int argc, char *argv[]);
+
+#endif
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "cpstd.h"
+#include "add.h"
 
 int main(int argc, char const *argv[])
 {
     if (argc <= 2) printf("Usage : command argument(s)");
     if (argv[0] == "cpstd") cp_file(argc, argv);
+    if (argv[0] == "append") append_to_file(argc, argv);
     return 0;
 }
